185b821ecf0844574bc59c7fdd47878fea465196
Darwin Reynolds <darwinreynolds2042@gmail.com>
2026-01-03 16:40:16 -0500

    add: names principals corrected in hero

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -8,7 +8,7 @@ export function HeroSection() {
   return (
     <section className="py-20 px-4 relative overflow-hidden min-h-screen flex flex-col justify-between">
       <div className="flex-1 flex items-start justify-center pt-20">
-        <ParticleTextEffect words={["LeLo", "SAAS", "PLATFORM", "LELO"]} />
+        <ParticleTextEffect words={["Darwin Reynolds", "ADSA", "PORTAFOLIO", "Darwin Reynolds"]} />
       </div>
 
       <div className="container mx-auto text-center relative z-10 pb-8">
